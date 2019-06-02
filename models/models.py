462c5aa8c037d24b3cf4642b2bf411ed20facbab
Shaheen Hossain <rapidgrps@princegroup-bd.com>
2019-06-02 17:32:13 +0600

    from fix number_next

--- models/models.py
+++ models/models.py
@@ -4,8 +4,10 @@ from odoo import models, fields, api
 
 class OnlineBookshop(models.Model):
      _name = 'online.bookshop'
+     #_inherit = ['product.template']
 
      book_name = fields.Char(string = "Book Name")
+     book_name_b = fields.Char(string = "Book Name in Bangla")
      book_image = fields.Binary(string = "Book Image")
 
      book_id = fields.Char(string="Book No.", readonly=True, required=True, copy=False, default='New')
@@ -17,13 +19,3 @@ class OnlineBookshop(models.Model):
           result = super(OnlineBookshop, self).create(vals)
           return result
 
-     # #application_no = fields.Char(string='Application  No', required=True, copy=False, readonly=True,
-     #  #                            index=True, default=lambda self: _('New'))
-     #
-     # @api.model
-     # def create(self, vals):
-     #    """Overriding the create method and assigning the the sequence for the record"""
-     #    if vals.get('book_id', _('New')) == _('New'):
-     #        vals['book_id'] = self.env['ir.sequence'].next_by_code('online.bookshop') or _('New')
-     #    res = super(OnlineBookshop, self).create(vals)
-     #    return res
